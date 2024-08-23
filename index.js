e72f7c308b1ebc7d21c5ffc34ecde11c678f357b
muthyaldev123 <pulikantimuthyalreddy@gmail.com>
2024-08-23 22:13:02 +0530

    question

---
+++ index.js
@@ -0,0 +1,130 @@
+
+What is the result of 10 + '5'?
+x=10+'5'
+console.log(x);  Ans:105
+What is the value of x after x = 10; x += 5;?
+x=10;
+x+=5;
+console.log(x);  Ans:15
+3. What is the result of 10 - null?
+x=10;
+y=null;
+z=x,y;
+console.log(z);  Ans:10
+What is the value of x after x = 10; x *= 2;?
+x=10;
+x*=2;
+console.log(x);   Ans:20
+What is the result of 10 / '2'?
+x=10/'2';
+console.log(x);   ANS:5
+What is the result of 10 % 3?
+
+
+console.log(10%3);
+x=10 % 3;
+console.log(x);    Ans:1
+7. What is the value of x after x = 10; x = x ** 2;?
+x=10;
+x=x ** 2;
+console.log(x);      Ans:100 
+8. What is the result of 10 > 5 && 5 < 10?
+x=10 > 5 && 5 < 10;
+console.log(x);    Ans: true 
+9. What is the result of 10 === '10'?
+x=10 === '10'; 
+console.log(x);  Ans:flase 
+10. What is the result of null ?? 'default'?
+x='default';
+console.log(x);  Ans: default
+11. What is the result of 10 || 'default'?
+x=10 || 'default';
+console.log(x);   Ans :10  
+12. What is the result of !true?
+x=!true;
+console.log(x);   Ans:flase  
+13. What is the result of 10 & 5?
+x=10 & 5;
+console.log(x);   Ans :0 
+14. What is the result of 10 | 5?
+x=10 | 5;
+console.log(x);  Ans :15
+15. What is the result of 10 ^ 5?
+x=10 ^ 5;
+console.log(x);  Ans :15 
+What is the result of x = 10; y = ++x;? What are the values of x and y?
+x= 10;
+y=++x;
+console.log(x,y);   Ans: 11,11
+2. What is the result of x = 10; y = x++;? What are the values of x and y?
+x= 10;
+y=x++;
+console.log(x,y);    Ans :11,10
+3. What is the result of x = 10; console.log(++x);?
+x=10;
+console.log(++x);   Ans :11
+4. What is the result of x = 10; console.log(x++);?
+x=10;
+console.log(x++);  Ans :10
+5. What is the result of x = 10; y = --x;? What are the values of x and y?
+x=10;
+y= --x;
+console.log(x,y);   Ans :9,9
+6. What is the result of x = 10; y = x--;? What are the values of x and y?
+x=10;
+y=x--;
+console.log(x,y);   Ans :9,10
+1. What is the result of x = 10; y = x > 5 ? 'yes' : 'no';? What is the value of y?
+x=10;
+y=x>5?'yes':'no';
+console.log(y);    Ans :yes 
+2. What is the result of x = 10; y = x > 5 ? 'yes' : x > 3 ? 'maybe' : 'no';? What is the value of y?
+x=10;
+y=x>5?'yes':x>3?'maybe':'no';
+console.log(y);  Ans :yes 
+1. What is the result of const obj = { a: { b: 'c' } }; console.log(obj?.a?.b);?
+obj ={a:{b:'c'}};
+console.log(obj?.a?.b);  Ans :c 
+2. What is the result of const obj = { a: { b: 'c' } }; console.log(obj?.a?.c);?
+obj={a:{b:'c'}};
+console.log(obj?.a?.c);    Ans ;undefined 
+3. What is the result of const obj = { a: { b: 'c' } }; console.log(obj?.b?.c);?
+obj={a:{b:'c'}};
+console.log(obj?.b?.c);    Ans :undefined
+1. What is the result of x = null; y = x ?? 'default';? What is the value of y?
+x=null;
+y=x;
+z='default';
+console.log(y);   Ans :null 
+2. What is the result of x = undefined; y = x ?? 'default';? What is the value of y?
+x=undefined;
+y=x??'default';
+console.log(y);  Ans :default 
+3. What is the result of x = 0; y = x ?? 'default';? What is the value of y?
+x=0;
+y=x??'default';
+console.log(y);  Ans :0  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
